d72f2aaaae26d8575fefed2cbf9b82fdf87e2936
andersonguse <andersonguse53469@gmail.com>
2026-06-14 17:40:20 -0400

    first commit

---
+++ workout-api/src/test/java/com/Anderson/workout_api/WorkoutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Anderson.workout_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
